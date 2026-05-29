9314362f192b44b996494f7a03853416a1c0cfef
renato <renato.natale.2004@gmail.com>
2026-05-30 01:38:20 +0200

    CODE: "Compilatore con il lexer quasi fatto"

--- code/Compilatore.py
+++ code/Compilatore.py
@@ -1,26 +1,71 @@
 import lark
 from lark import Lark, grammar
 
-Grammar = (r"""
-           //tipo di dati
+
+
+def compilatore(source: str) -> str:
+   Grammar = (r"""
+               //tipo di dati
            
-           BOOLEAN : /"true"|"false"/
-           NUMR: /\d+?(.\d+)/
-           NBRUGGLIO: /\w+/
-           ID: /[a-zA-Z_]\w*/ 
-           LETTR: [/w]
-           VACANT = "void"
-           
-           MEN: "+"
-           PIU: "-"           
-           DIVIS: "*"
-           MOLTIP: "/"
-           ASSIGN: "="
-           
-           MAGGIOR: ">"
-           MINOR: "<"
-           MAGGIORUGUALE: ">="
-           MINORUGUALE: 
-           %ignore: /\s+/
+               BOOLEAN : /"true"|"false"/
+               NUMR: /\d+?(.\d+)/
+               NBRUGGLIO: /\w+/
+               ID: /[a-zA-Z_]\w*/ 
+               LETTR: [/w]
+               VACANT = "void"
+               
+              //operazioni
+               MEN: "+"
+               PIU: "-"           
+               DIVIS: "*"
+               MOLTIP: "/"
+               ASSIGN: "="
+               SOMMASSIGN: "-="
+               MENASSIGN: "+="
+               DIVISASSIGN: "*="
+               PERASSIGN: "/="
+               
+              //operazioni logiche          
+               MAGGIOR: ">"
+               MINOR: "<"
+               MAGGIORUGUALE: ">="
+               MINORUGUALE: "<="
+               EQUALS: "=="
+               DIVERS: "!="
+               
+              //PARENTESSI 
+               TONDASINISTRA: ")"
+               TONDADESTRA: "("
+               GRAFFASINISTRA: "}"
+               GRAFFADESTRA: "{"
+               QUADRATASINISTRA: "]"
+               QUADRATADESTRA: "["
+               
+               //Keyword
+               METTIMCA: "mettimcà"
+               ALLORFAACCUSSI: "allor_fa_accussi"
+               
+               
+               //classi
+               ROBA: "roba"
+               
+               //cicli
+               ASPE: "aspe"
+               MARONN: "maronn"
+               
+               //funzione
+               MESTIER: "mestier"
+               
+               //utilizzi generali
+               VIRGULET: "\\?"
+               NUNCSTANIENT: "NULL"
+               SCCASCIA: "sccascià"
+               CCASTA: "ccàsta"
+               
+               %ignore: /\s+/
+               %ignore: /\/\/[^\n]*/
+               %ignore: /\/\*[\s\S]*\*\//
+            
+                
            """
-)
+    )
