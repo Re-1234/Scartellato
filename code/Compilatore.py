b9265c17d38a779ef528b450cc7e0840653917b2
StefanoGit50 <s.santoro41@studenti.unisa.it>
2026-06-07 17:11:33 +0200

    CODE: aggiunta gestione errori su command line

--- code/Compilatore.py
+++ code/Compilatore.py
@@ -1,4 +1,4 @@
-from lark import Lark
+from lark import Lark, UnexpectedToken, UnexpectedCharacters
 
 
 def compilatore(source: str) -> str:
@@ -7,8 +7,20 @@ def compilatore(source: str) -> str:
     parser = Lark.open("grammatica.lark",parser="lalr")
     for token in parser.lex(source):
         print(token)
-    tree=parser.parse(source)
-    print(tree.pretty())
+
+    """gestione degli errori"""
+    try:
+        tree = parser.parse(source)
+        print(tree.pretty())
+    except UnexpectedToken as e:
+        print(f"Errore sintattico alla riga {e.line}, col {e.column}")
+        print(f"Token inatteso: {e.token!r}")
+        print(f"Token attesi: {e.expected}")
+        print(e.get_context(source))
+    except UnexpectedCharacters as e:
+        print(f"Errore lessicale: {e.char!r}")
+
+
 
 compilatore("""maradona ) nbruogglio ] [ args ( }
         nbruogglio a = ??sifasf23??!
