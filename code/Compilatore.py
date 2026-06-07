7f01dbb4ef669fc2b14c5777e25241e0b566189e
renato <renato.natale.2004@gmail.com>
2026-06-07 11:40:57 +0200

    CODE: "aggiornamento del lexer tolto il token : VIRGULET"

--- code/Compilatore.py
+++ code/Compilatore.py
@@ -3,7 +3,7 @@ from lark import Lark
 
 def compilatore(source: str) -> str:
 
-
+    
     parser = Lark.open("grammatica.lark",parser="lalr")
     for token in parser.lex(source):
         print(token)
