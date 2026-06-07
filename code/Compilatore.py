090243b819d74b3fa879f70cd38c38beeabd2797
StefanoGit50 <s.santoro41@studenti.unisa.it>
2026-06-07 14:09:09 +0200

    CODE: inizio parsing

--- code/Compilatore.py
+++ code/Compilatore.py
@@ -7,5 +7,10 @@ def compilatore(source: str) -> str:
     parser = Lark.open("grammatica.lark",parser="lalr")
     for token in parser.lex(source):
         print(token)
+    tree=parser.parse(source)
+    print(tree.pretty())
 
-compilatore("nbruogglio a = ??sifasf23??!")
+compilatore("""maradona ) nbruogglio ] [ args ( }
+        nbruogglio a = ??sifasf23??!
+    {
+""")
